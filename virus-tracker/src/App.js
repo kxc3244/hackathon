9de930c36d40b356b3398af995a815ed2977e5f5
Luis Camacho <luiscamachojr1@gmail.com>
2020-03-07 14:58:34 -0600

    installed bootstrap

--- virus-tracker/src/App.js
+++ virus-tracker/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 //import logo from './logo.svg';
+import 'bootstrap';
 import './App.css';
+ // eslint-disable-next-line
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
